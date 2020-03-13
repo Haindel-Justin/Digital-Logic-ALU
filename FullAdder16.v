56b29812a54125a15942b152586bbfdd4c47e6dd
Abdus Quadri <asquadri1@gmail.com>
2020-03-13 17:33:03 -0500

    removed commented out code

--- FullAdder16.v
+++ FullAdder16.v
@@ -169,69 +169,6 @@ module testbench();
   $display("||A    ||B    ||Cin||A+B||Cout");
   $display("||%b||%b||%b||%b||%b",i,j,1'b0,f1,outwire);
 
-/*	 
-	
-  //$display acts like a classic C printf command.
-
-  $display ("|  |s|s|s|    |    |    |    |    |    |    |    ||    |LA  |E   |"); 
-  $display ("| #|2|1|0|Ch 7|Ch 6|Ch 5|Ch 4|Ch 3|Ch 2|Ch 1|Ch 0||b   | out| out|");
-  $display ("|==+=+=+=+====+====+====+====+====+====+====+====++====+====+====+");
-  
-    //A for loop, with register i being the loop control variable.
-	for (i = 0; i < 65535; i = i + 1) 
-	 //always@(*)
-	 begin//Open the code blook of the for loop
-		s15=(i/32768)%2;
-		s14=(i/16384)%2;
-		s13=(i/8192)%2;
-		s12=(i/4096)%2;
-		s11=(i/2048)%2;
-		s10=(i/1024)%2;
-		s9=(i/512)%2;
-		s8=(i/256)%2;
-		s7=(i/128)%2;
-		s6=(i/64)%2;
-		s5=(i/32)%2;
-		s4=(i/16)%2;
-		s3=(i/8)%2;
-		s2=(i/4)%2;
-		s1=(i/2)%2;
-		s0=(i/1)%2;	
-		
-		
-		
-		for (j = 0; j < 65535; j = j + 1)  
-	 		begin
-		w15=(j/32768)%2;
-		w14=(j/16384)%2;
-		w13=(j/8192)%2;
-		w12=(j/4096)%2;
-		w11=(j/2048)%2;
-		w10=(j/1024)%2;
-		w9=(j/512)%2;
-		w8=(j/256)%2;
-		w7=(j/128)%2;
-		w6=(j/64)%2;
-		w5=(j/32)%2;
-		w4=(j/16)%2;
-		w3=(j/8)%2;
-		w2=(j/4)%2;
-		w1=(j/2)%2;
-		w0=(j/1)%2;
-
-		#5;
-		
-
-
-		
-		$display ("|%5d|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b||||||||%5d|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|%1b|||||||||||%5d|%16b|",i,s15,s14,s13,s12,s11,s10,s9,s8,s7,s6,s5,s4,s3,s2,s1,s0,j,w15,w14,w13,w12,w11,w10,w9,w8,w7,w6,w5,w4,w3,w2,w1,w0,f1,f1,outwire);
-		if(j%4==3) //Every fourth row of the table, put in a marker for easier reading.
-		 $display ("|==+=+=+=+====+====+====+====+====+====+====+====++====+====+====+");
-  		
-		end
-  
-	end//End of the for loop code block
- */
 	#10 //A time dealy of 10 time units. Hashtag Delay
 	$finish;//A command, not unlike System.exit(0) in Java.
   end  //End the code block of the main (initial)
