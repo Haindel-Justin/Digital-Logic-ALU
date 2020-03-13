74cb08e9fd51038242c821775eca845e3937dd2c
Raufil <52762415+raufildhuka@users.noreply.github.com>
2020-03-13 18:03:10 -0500

    Add files via upload

---
+++ NAND.v
@@ -0,0 +1,80 @@
+module Gate_NAND(a,b,r);
+parameter size=16;
+input [size-1:0] a;
+input [size-1:0] b;
+output [size-1:0] r;
+assign r =  ~(a & b);
+endmodule
+module testbench();
+
+  //Registers act like local variables
+  reg [15:0] i; //A loop control that can go to 16 to handle a less than.
+  reg  s15;
+  reg  s14;
+  reg  s13;
+  reg  s12;
+  reg  s11;
+  reg  s10;
+  reg  s9;
+  reg  s8;
+  reg  s7;
+  reg  s6;
+  reg  s5;
+  reg  s4;
+  reg  s3;
+  reg  s2;
+  reg  s1;
+  reg  s0;
+  reg [15:0] j; 
+  reg  w15;
+  reg  w14;
+  reg  w13;
+  reg  w12;
+  reg  w11;
+  reg  w10;
+  reg  w9;
+  reg  w8;
+  reg  w7;
+  reg  w6;
+  reg  w5;
+  reg  w4;
+  reg  w3;
+  reg  w2;
+  reg  w1;
+  reg  w0;
+ 
+  
+  //A wire can hold the return of a function
+  wire  outwire;
+  wire  [15:0] f1;
+  wire  outwire2;
+  wire  [15:0] f2;
+
+  wire  [15:0] ft;
+  wire  outwire3;
+  wire  [15:0] f3;
+
+  wire  outwire4;
+  wire  outwire5;
+  wire  outwire6;
+ 	
+  
+
+   Gate_NAND xr(i,j,f1);
+
+
+  //Initial means "start," like a Main() function.
+  //Begin denotes the start of a block of code.	
+ 
+  initial begin
+   	i=248;
+	j=327;
+	#5;
+	$display("|   i     |     j    | NAND");
+	$display("|%b|%b |   %b",i,j,f1);
+
+ 
+	#10; //A time dealy of 10 time units. Hashtag Delay
+	$finish;//A command, not unlike System.exit(0) in Java.
+  end  //End the code block of the main (initial)
+  endmodule
